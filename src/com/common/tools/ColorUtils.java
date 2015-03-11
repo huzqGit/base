a377c08515c7c6f61a5c891ae863240539840cff
Administrator <Administrator@UC0RYW2MZORPQD1.lan>
2015-03-11 10:53:53 +0800

    test

--- src/com/common/tools/ColorUtils.java
+++ src/com/common/tools/ColorUtils.java
@@ -7,13 +7,11 @@ public class ColorUtils {
 		String r, g, b;
 		Random random = new Random();
 		r = Integer.toHexString(random.nextInt(256)).toUpperCase();
-		g = Integer.toHexString(random.nextInt(256)).toUpperCase();
 		b = Integer.toHexString(random.nextInt(256)).toUpperCase();
-
+		String d = "";
 		r = r.length() == 1 ? "0" + r : r; 
-		g = g.length() == 1 ? "0" + g : g;
 		b = b.length() == 1 ? "0" + b : b;
 
-		return r + g + b;
+		return r + b;
 	}
 }
