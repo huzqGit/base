f69715abb5842eb846d15d7ec2d6add94fbec10f
Administrator <Administrator@UC0RYW2MZORPQD1.lan>
2015-03-11 10:48:59 +0800

    test

--- src/com/common/tools/ColorUtils.java
+++ src/com/common/tools/ColorUtils.java
@@ -10,7 +10,7 @@ public class ColorUtils {
 		g = Integer.toHexString(random.nextInt(256)).toUpperCase();
 		b = Integer.toHexString(random.nextInt(256)).toUpperCase();
 
-		r = r.length() == 1 ? "0" + r : r;
+		r = r.length() == 1 ? "0" + r : r; 
 		g = g.length() == 1 ? "0" + g : g;
 		b = b.length() == 1 ? "0" + b : b;
 
